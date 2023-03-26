7855820c096e623aa44dd2bcbfdab268bf6eff75
RakotosonAndrianina <88959960+RakotosonAndrianina@users.noreply.github.com>
2023-03-26 12:39:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const card1 = document.querySelector(".card1");
+const card2 = document.querySelector('.card2');
+const card3 = document.querySelector(".card3");
+const card4 = document.querySelector('.card4');
+const petiText = document.querySelector(".petit-text")
+const toggleText = document.querySelector(".toggle-text")
+const granChef1 = document.querySelector(".grand1");
+const granChef2 = document.querySelector(".grand2");
+const granChef3 = document.querySelector(".grand3");
+        console.log(right)
+        window.addEventListener("scroll", () =>{
+            console.log(window.scrollY);
+            if(window.scrollY > 120){
+                left.style.transform = "translateX(0)";
+                left.style.opacity = "1";
+            }else{
+                // left.style.transform = "translateX(-200%)";
+                left.style.opacity = "0";
+            }
+        })
+
+        window.addEventListener("scroll", () =>{
+            console.log(window.scrollY);
+            if(window.scrollY > 120){
+                right.style.transform = "translateX(0)";
+                right.style.opacity = "1";
+            }else{
+                // right.style.transform = "translateX(+200%)";
+                right.style.opacity = "0";
+            }
+        })
+
+
+        card1.addEventListener("click", () =>{
+            // petiText.classList.toggle("clicked");
+            petiText.style.transform = "translateY(-200px)";
+        })
+
+
+        window.addEventListener('scroll', () =>{
+            if(window.scrollY > 1950){
+                granChef1.style.transform = "translateY(0)";
+                granChef1.style.opacity = "1";
+            }else{
+                
+            }
+        })
+
+        window.addEventListener('scroll', () =>{
+            if(window.scrollY > 1990){
+                granChef2.style.transform = "translateY(0)";
+                granChef2.style.opacity = "1";
+            }else{
+            }
+        })
+
+
+        window.addEventListener('scroll', () =>{
+            if(window.scrollY > 2050){
+                granChef3.style.transform = "translateY(0)";
+                granChef3.style.opacity = "1";
+            }else{
+                
+            }
+        })
+
+
